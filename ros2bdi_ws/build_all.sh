c1603b557b091210f353cc3b8db342d2a65c5994
Marco Robol <marco.robol@unitn.it>
2024-02-29 15:46:35 +0100

    JavaFF reverted to original structure and squashed in one commit; setup .repos with repos from ros2 ros2_java and colcon

--- ros2bdi_ws/build_all.sh
+++ ros2bdi_ws/build_all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cd ./src
+vcs import < ros2_java_desktop.repos
+
 NUM_CORES=$(nproc) && \
 export MAKEFLAGS="-j$NUM_CORES" && \
 export AMENT_BUILD_GRADLE_ARGS="-j$NUM_CORES" && \
@@ -10,42 +13,47 @@ export AMENT_BUILD_MAKE_ARGS="-j$NUM_CORES"
 apt-get update
 
 rosdep install -y -r -q --from-paths src --ignore-src --rosdistro ${ROS_DISTRO}
+rosdep install --from-paths src -y -i --skip-keys "ament_tools" # as specified at https://github.com/ros2-java/ros2_java
 
 
-
-# ---- plansys2 ------------
+# ---- from ros2_planning_system ------------
 colcon build --symlink-install --base-paths src/ros2_planning_system --packages-ignore plansys2_bt_actions 2>&1 | tee ./plansys.build.log
 
-# ---- javaff ------------
-colcon build --symlink-install --packages-select ament_java_resources ament_build_type_gradle ament_cmake_export_jars ament_cmake_export_jni_libraries rcljava_common rosidl_generator_java
+# ---- from ros2 --------------------------
+colcon build --symlink-install --base-paths src/ros2
+colcon build --symlink-install --packages-select rcljava #--base-paths src/ros2_java
+colcon build --symlink-install --base-paths src/ros2_java
+# colcon build --symlink-install --packages-select ament_java_resources ament_build_type_gradle ament_cmake_export_jars ament_cmake_export_jni_libraries rcljava_common rosidl_generator_java
+# colcon build --symlink-install --packages-select rosidl_default_generators rosidl_default_runtime builtin_interfaces std_msgs
+# colcon build --symlink-install --base-paths /opt/ros/humble --packages-select builtin_interfaces std_msgs
 
-# ---- javaff ------------
-colcon build --symlink-install --packages-select rosidl_default_generators rosidl_default_runtime builtin_interfaces std_msgs
+# ---- from JavaFF --------------------------
+colcon build --symlink-install --base-paths src/JavaFF --event-handlers console_direct+
 
-# ---- plansys2 ------------
-colcon build --symlink-install --packages-select plansys2_msgs unique_identifier_msgs action_msgs
+# ---- from ros2_planning_system ------------
+colcon build --symlink-install --packages-select plansys2_msgs
 
-# ---- ros2bdi ------------
+# ---- from ROS2-BDI ------------------------
 colcon build --symlink-install --packages-select ros2_bdi_interfaces
 # rm -rf /root/ros2bdi_ws/build/ros2_bdi_interfaces
 
-# ---- javaff ------------
+# ---- from JavaFF --------------------------
 colcon build --symlink-install --packages-select javaff_interfaces --packages-ignore unique_identifier_msgs action_msgs
 
-# ---- ros2bdi ------------
+# ---- from ROS2-BDI ------------------------
 colcon build --packages-select ros2_bdi_utils ros2_bdi_skills ros2_bdi_bringup ros2_bdi_core --packages-skip ros2_bdi_interfaces
 
-# ---- ros2bdi ------------
+# ---- from ROS2-BDI ------------------------
 colcon build --symlink-install --packages-ignore ros2_bdi_tests --packages-skip ros2_bdi_interfaces ros2_bdi_utils ros2_bdi_skills ros2_bdi_bringup ros2_bdi_core
 
-# ---- ros2bdi ------------
+# ---- from ROS2-BDI ------------------------
 source /root/ros2bdi_ws/install/setup.bash
 colcon build --symlink-install --packages-select ros2_bdi_tests
 
-# ---- ros2bdi ------------
+# ---- from ROS2-BDI ------------------------
 # colcon build --symlink-install --packages-select webots_ros2_simulations_interfaces webots_ros2_simulations ros2_bdi_on_webots
 
-# ---- javaff ------------
+# ---- from JavaFF --------------------------
 colcon build --symlink-install --base-paths src/JavaFF --packages-skip javaff_interfaces ament_java_resources ament_build_type_gradle ament_cmake_export_jars ament_cmake_export_jni_libraries rcljava_common rosidl_generator_java
 
 
