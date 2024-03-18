87a914b048ef7c6f8cdff49ba1d68eb4c8c081b9
Marco Robol <marco.robol@unitn.it>
2024-03-18 12:22:06 +0100

    Compile with no errors, finally

--- ros2bdi_ws/build_all.sh
+++ ros2bdi_ws/build_all.sh
@@ -18,34 +18,34 @@ export AMENT_BUILD_MAKE_ARGS="-j$NUM_CORES"
 apt-get update
 # rosdep install -y -r -q --from-paths src --ignore-src --rosdistro ${ROS_DISTRO}
 rosdep install --from-paths src src-java -y -i --skip-keys "ament_tools" # as specified at https://github.com/ros2-java/ros2_java
-source /opt/ros/humble/setup.bash
 
-# ---- from ros2_planning_system ------------
+# ---- from src/ros2_planning_system ----------
+source ./install/setup.bash
 colcon build --symlink-install --base-paths src/ros2_planning_system --event-handlers console_direct+ --packages-ignore plansys2_bt_actions 2>&1 | tee ./plansys.build.log
 
-# ---- from ros2 --------------------------
+# ---- from src-java --------------------------
+source ./install/setup.bash
 # apt-get install ros-humble-rcl* # non serve
 colcon build --symlink-install --base-paths src-java # to compile ros2-java up to rcljava; DO NOT WORK specifying the rcljava package or the folder
 # colcon build --symlink-install --packages-up-to rcljava # non funziona, non trova rcl_interfaces
 
-# ---- from JavaFF --------------------------
+# ---- from src/JavaFF ------------------------
 source ./install/setup.bash
 colcon build --symlink-install --base-paths src/JavaFF --event-handlers console_direct+
 
-# ---- from ROS2-BDI ------------------------
+# ---- from src/ROS2-BDI ----------------------
+source ./install/setup.bash
 colcon build --symlink-install --packages-select ros2_bdi_interfaces
 # rm -rf ./build/ros2_bdi_interfaces
 
-# ---- from javaff_interfaces ---------------
+# ---- from src/javaff_interfaces -------------
 source ./install/setup.bash
 colcon build --symlink-install --base-paths src/javaff_interfaces
 # colcon build --symlink-install --packages-select javaff_interfaces --packages-ignore unique_identifier_msgs action_msgs
 
-# ---- from ROS2-BDI ------------------------
-colcon build --packages-select ros2_bdi_utils ros2_bdi_skills ros2_bdi_bringup ros2_bdi_core --packages-skip ros2_bdi_interfaces
-##### UP-TO-HERE #####
-colcon build --symlink-install --packages-ignore ros2_bdi_tests --packages-skip ros2_bdi_interfaces ros2_bdi_utils ros2_bdi_skills ros2_bdi_bringup ros2_bdi_core
-# colcon build --symlink-install --packages-ignore ros2_bdi_tests --packages-skip ros2_bdi_interfaces
+# ---- from src/ROS2-BDI ----------------------
 source ./install/setup.bash
+colcon build --packages-select ros2_bdi_utils ros2_bdi_skills ros2_bdi_bringup ros2_bdi_core --packages-skip ros2_bdi_interfaces
+# colcon build --symlink-install --packages-select litter_world ros2_bdi_on_litter_world litter_world_interfaces webots_ros2_simulations_interfaces webots_ros2_simulations ros2_bdi_on_webots
+colcon build --symlink-install --base-paths src/ROS2-BDI --packages-ignore ros2_bdi_tests --packages-skip ros2_bdi_interfaces ros2_bdi_utils ros2_bdi_skills ros2_bdi_bringup ros2_bdi_core
 colcon build --symlink-install --packages-select ros2_bdi_tests
-# colcon build --symlink-install --packages-select webots_ros2_simulations_interfaces webots_ros2_simulations ros2_bdi_on_webots
